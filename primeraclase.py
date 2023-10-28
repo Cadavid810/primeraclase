d7573417c347e658001aa43ce753958cf6a10d9c
Cadavid810 <mael-8@hotmail.com>
2023-10-28 12:03:39 -0500

    calcular el área de un cuadrado

--- primeraclase.py
+++ primeraclase.py
@@ -1,1 +1,18 @@
-primeraclase.py
+number = 5 
+
+print('El valor de la variable es:', number)
+
+texto = 'texto'
+
+float = 5.6
+
+sum = number + float
+
+print (sum)
+
+base = int(input('ingrese altura'))
+altura = int(input('ingrese la base'))
+
+resultado = base * altura 
+
+print('el area de un cuadrado es:', resultado)
